6069e7af7fab0808a403585cc2aeb62721d45920
Loris Leuenberger <loris.n.leuenberger@gmail.com>
2025-12-04 18:55:09 +0100

    WIP ab01 excercise 4

---
+++ ab01/04_lotto/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const int amountOfTips = 6;
+        int[] tips = new int[amountOfTips];
+        int[] results = new int[amountOfTips];
+
+
+        Console.WriteLine("Bitte geben Sie ihre Tipps ab (1-45)");
+        for(int i = 1; i <= amountOfTips; i++)
+        {
+            tips[i] = ReadTip(i);
+        }
+        
+        for(int i = 1; i <= amountOfTips; i++)
+        {
+            results[i] = Random.Next(1, 45);
+        }
+
+        for(int i = 0; i < tips.Count(); i++)
+        {
+            if(tips[i] == results[i])
+            {
+                Console.WriteLine("asdf");
+            }
+        }
+    }
+    
+    private static int ReadTip(int index)
+    {
+        Console.Write("Tipp " + index + ": ");
+        int tip;
+        string input = Console.ReadLine();
+        if (int.TryParse(input, out tip))
+        {
+            return tip;
+        }
+        Console.WriteLine("Error: Seems like you didn't write a number. Let's try again.");
+        return ReadTip(index);
+    }
+}
